f303eabc7133c40730a4a4455d2f6d733e5221bd
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2022-03-14 17:26:37 -0300

    Added Delivery

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController'
 import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController'
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController'
+import { CreateDeliveryController } from './modules/deliveries/createDelivery/CreateDeliveryController'
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController'
 
 const routes = Router()
@@ -10,6 +11,8 @@ const createDeliverymanController = new CreateDeliverymanController()
 const authenticateClientController = new AuthenticateClientController()
 const authenticateDeliverymanController =
   new AuthenticateDeliverymanController()
+
+const createDeliveryController = new CreateDeliveryController()
 
 routes.post('/client/', createClientController.handle)
 
@@ -20,5 +23,7 @@ routes.post(
   '/deliveryman/authenticate',
   authenticateDeliverymanController.handle
 )
+
+routes.post('/delivery', createDeliveryController.handle)
 
 export { routes }
